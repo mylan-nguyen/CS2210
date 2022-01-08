c0360970a38f73af322b1e352b10131104c82d1a
Mylan Nguyen <86733306+mylan-nguyen@users.noreply.github.com>
2022-01-08 13:23:38 -0500

    Create MostTimes.java

---
+++ Assignment 1/MostTimes.java
@@ -0,0 +1,10 @@
+public class MostTimes {
+
+    public int most_times(int[] A, int n){
+    // Input: Array A storing n  integer values
+    // Output: The value that appears the largest number of times in $A$. If several values appear
+    //         in $A$ the largest number of times, the algorithm must return the smalles among these
+    //         values.
+
+    }
+}
